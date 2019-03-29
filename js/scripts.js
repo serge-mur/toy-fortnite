ea972ba4e8d0a540d6d3b85fc065fc579573115c
Sergei <serge-mur@yandex.ru>
2019-03-29 11:18:56 +0300

    install blank tpl

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $('#offCanvas').offcanvas({
+        modifiers: 'left, overlay',
+        triggerButton: '#triggerButton'
+    });
+
+    $('#offCanvasRight').offcanvas({
+        modifiers: 'right, overlay',
+        triggerButton: '#triggerButtonRight'
+    });
+
+    $('.main-slider').slick({
+
+    });
+
+    $('.responsive-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+
+    if (window.matchMedia("screen and (min-width: 1px) and (max-width:575px)").matches) {
+        console.log('mobile');
+    }
+
+});
