108f69ab5b6c141e7a2277e705a06c7dfc9315be
Sergei <serge-mur@yandex.ru>
2019-04-01 16:47:33 +0300

    verstka... day 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,50 @@
 $(document).ready(function () {
 
+    $("#main-menu .nav-link").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+// back to top
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 50) {
+    //         $('#back-to-top').fadeIn();
+    //     } else {
+    //         $('#back-to-top').fadeOut();
+    //     }
+    // });
+    // // scroll body to 0px on click
+    // $('#back-to-top').click(function () {
+    //     event.preventDefault();          
+    //     $('body,html').animate({
+    //         scrollTop: 0
+    //     }, 800);
+    //     return false;
+    // });
+
+
     $('.main-slider').slick({
 
     });
 
-    $('.responsive-slider').slick({
+    $('.character-slider').slick({
+        fade: true,
+        prevArrow: $(".slick-prev-c"),
+        nextArrow: $(".slick-next-c")
+    });
+
+    $('.toys-slider').slick({
+        dots: true
+    });
+
+    $('.partners-slider').slick({
         dots: true,
         infinite: true,
         speed: 300,
-        slidesToShow: 6,
-        slidesToScroll: 6,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         responsive: [
             {
                 breakpoint: 992,
